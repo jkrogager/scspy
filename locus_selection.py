2924828915cfec4e47c1687dab269a33b8d4c027
Jens-Kristian Krogager <jens-kristian@krogager.dk>
2017-11-22 19:05:25 +0100

    Correcting data path and validation
    Getting the correct data path using the __file__ attribute.
    Also implemented data validation for the input arrays.

--- locus_selection.py
+++ locus_selection.py
@@ -1,25 +1,72 @@
 import numpy as np
 import locus_tools as lt
+from os import path
+
+root_path = path.dirname(path.abspath(__file__))
 
 
-def run_locus_selection(sample, errors, N_err=4, locus='ugri'):
+def run_locus_selection(colors, errors, N_err=4, locus='ugri'):
+    """
+    Test whether input colors are inside the stellar locus defined
+    in either ``ugri`` or ``griz`` color-spaces.
+
+    Parameters
+    ----------
+    colors : array_like, shape (N, 3)
+        An array of N color vectors consisting of 3 colors:
+        For ``ugri``: u-g, g-r, r-i ; and for ``griz``: g-r, r-i, i-z.
+
+    errors : array_like, shape (N, 3)
+        An array of N vectors containing the error on the input `colors`.
+
+    N_err : int   [default = 4]
+        The number of sigma used for the convolution of the stellar locus
+        with the error ellipsoid defined by `errors`.
+
+    locus : str   [default = 'ugri']
+        Which stellar locus to work on, must be either ``ugri`` or ``griz``.
+
+    Returns
+    -------
+    in_locus : array(bool), shape (N)
+        Boolean array of length N for each target in the input `colors`.
+        The array will be `True` if the target is inside the stellar locus
+        convolved by `N_err` times the `errors`.
+    """
+
+    colors = np.array(colors)
+    errors = np.array(errors)
+
+    # Check input data format:
+    assert colors.shape == errors.shape, "Input data and errors must have same dimensions."
+
+    if len(colors.shape) == 2:
+        if colors.shape[1] == 3:
+            pass
+        else:
+            err_msg = "Input only allows three colours. Not %r given."
+            raise ValueError(err_msg % colors.shape[1])
+    else:
+        err_msg = "Wrong input dimensions: %r"
+        raise ValueError(err_msg % colors.shape)
+
     # --- Load ugri Stellar Locus:
     # i     k       N      u-g     g-r     r-i    k_ug    k_gr    k_ri      a       b      theta
     if locus.lower() == 'ugri':
-        locus_pars = np.loadtxt('ugri_table_York2002.dat')
+        locus_pars = np.loadtxt(root_path+'/ugri_table_York2002.dat')
         # these are set manually for ugri and griz:
         a_k = 0.2
         k_end = -0.05
 
     elif locus.lower() == 'griz':
-        locus_pars = np.loadtxt('griz_table_York2002.dat')
+        locus_pars = np.loadtxt(root_path+'/griz_table_York2002.dat')
         # these are set manually for ugri and griz:
         a_k = 0.5
         k_end = -0.3
     O = locus_pars[:, 3:6]
 
     in_locus = list()
-    for c, e in zip(sample, errors):
+    for c, e in zip(colors, errors):
         loc_i = lt.find_nearest_locus_point(c, O)
 
         # --- Set locus parameters:
